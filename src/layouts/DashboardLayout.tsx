b24c0cfca996488a0feb72069ec934898900b221
emad <arabanemad@gmail.com>
2025-08-05 17:09:43 +0330

    update DashboardLayout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "@/modules/shared/components/header/Header";
+import Sidebar from "@/modules/shared/components/sidebar/SideBar";
+import Menu from "@/modules/shared/components/header/Menu";
 
 const DashboardLayout = () => {
-  return <div>DashboardLayout</div>;
+  return (
+    <div className="flex h-screen">
+      <div className="hidden md:block">
+        <Sidebar />
+      </div>
+
+      <div className="block md:hidden">
+        <Menu />
+      </div>
+
+      <div className="flex flex-col flex-1 ml-12 md:ml-4">
+        <Header />
+        <main className="p-4 overflow-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
