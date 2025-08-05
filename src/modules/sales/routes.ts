344bdf32439905ff797365eb93b375ce07b7f5bd
emad <arabanemad@gmail.com>
2025-08-05 17:07:48 +0330

    update sales/routes

--- src/modules/sales/routes.ts
+++ src/modules/sales/routes.ts
@@ -1,10 +1,47 @@
-import type { RouteObject } from "react-router-dom";
-import { SalePage } from '@/modules/sales';
+// import type { RouteObject } from "react-router-dom";
+// import { SalePage } from '@/modules/sales';
+// import React from "react";
+
+// export const salesRoutes: RouteObject[] = [
+//     {
+//         path: '/sales',
+//         element: React.createElement(SalePage),
+//     },
+// ];
+
+// import { SalePage } from "@/modules/sales";
+// import DashboardLayout from "@/layouts/DashboardLayout";
+// import React from "react";
+// import type { RouteObject } from "react-router-dom";
+
+// export const salesRoutes: RouteObject[] = [
+//     {
+//         path: "/dashboard",
+//         element: React.createElement(DashboardLayout),
+//         children: [
+//             {
+//                 index: true,
+//                 element: React.createElement(SalePage),
+//             },
+//         ],
+//     },
+// ];
+
+import { SalePage } from "@/modules/sales";
+import DashboardLayout from "@/layouts/DashboardLayout";
 import React from "react";
+import type { RouteObject } from "react-router-dom";
 
 export const salesRoutes: RouteObject[] = [
     {
-        path: '/sales',
-        element: React.createElement(SalePage),
+        path: "/dashboard",
+        element: React.createElement(DashboardLayout),
+        children: [
+
+            {
+                path: "sales", // Maps to /dashboard/sales
+                element: React.createElement(SalePage)
+            },
+        ],
     },
 ];
