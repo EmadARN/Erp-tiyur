08ecdb93c50ea57e0acd0c6c986be64476e4fd33
emad <arabanemad@gmail.com>
2025-08-06 13:01:36 +0330

    add settingDrawerWrapper

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,11 +1,15 @@
+import { useRef } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "@/modules/shared/components/header/Header";
 import Sidebar from "@/modules/shared/components/sidebar/SideBar";
 import Menu from "@/modules/shared/components/header/Menu";
+import SettingsDrawerWrapper from "@/modules/shared/components/header/settingDrawer/SettingsDrawerWrapper";
 
 const DashboardLayout = () => {
+  const dashboardRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div className="flex h-screen">
+    <div ref={dashboardRef} className="flex h-screen">
       <div className="hidden md:block">
         <Sidebar />
       </div>
@@ -14,12 +18,14 @@ const DashboardLayout = () => {
         <Menu />
       </div>
 
-      <div className="flex flex-col flex-1 ml-12 md:ml-4">
+      <div className="flex flex-1 flex-col ml-12 md:ml-4">
         <Header />
         <main className="p-4 overflow-auto">
           <Outlet />
         </main>
       </div>
+
+      <SettingsDrawerWrapper dashboardRef={dashboardRef} />
     </div>
   );
 };
