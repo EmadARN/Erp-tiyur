c464f5ab21d04e6b6dbcd8833facd219ed29ffc4
emad <arabanemad@gmail.com>
2025-08-07 12:07:21 +0330

    fixed initialize .envType

--- src/modules/shared/types/custom.d.ts
+++ src/modules/shared/types/custom.d.ts
@@ -3,3 +3,15 @@ declare module '*.css' {
     const content: { [className: string]: string }
     export default content
 }
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+    // می‌تونی سایر env ها رو هم اینجا اضافه کنی مثل:
+    // readonly VITE_APP_VERSION: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
