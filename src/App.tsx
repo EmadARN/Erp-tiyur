17cc3d18b55eeb10a30dc476dcdeadf04aa51c30
RezaFarzipour <rezafafa78@gmail.com>
2025-08-08 17:06:45 +0330

    toaster addded to app.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./router";
-
+import { Toaster } from 'react-hot-toast';
 const App = () => {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <Toaster position="top-center" />
     </BrowserRouter>
   );
 };
