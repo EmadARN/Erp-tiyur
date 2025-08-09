e84f688439d09293857c7a72b0811387f252a09e
emad <arabanemad@gmail.com>
2025-08-09 12:22:59 +0330

    update Switch

--- src/modules/shared/components/ui/Switch.tsx
+++ src/modules/shared/components/ui/Switch.tsx
@@ -1,11 +1,10 @@
 import React from "react";
+import { useThemeSettings } from "../../hooks/useThemeSettings";
 
 type SwitchProps = {
   checked: boolean;
   onChange: (checked: boolean) => void;
   label?: string;
-
-  // کلاس‌های سفارشی برای بخش‌های مختلف
   containerClassName?: string;
   labelClassName?: string;
   trackClassName?: string;
@@ -21,12 +20,17 @@ const Switch: React.FC<SwitchProps> = ({
   trackClassName = "",
   thumbClassName = "",
 }) => {
+  // get rtl state from redux
+  const { rtl } = useThemeSettings();
+
   return (
     <label
       className={`flex items-center gap-2 cursor-pointer select-none ${containerClassName}`}
     >
       {label && (
-        <span className={`text-sm text-gray-700 ${labelClassName}`}>
+        <span
+          className={`text-sm text-gray-700 dark:text-gray-200 ${labelClassName}`}
+        >
           {label}
         </span>
       )}
@@ -34,15 +38,21 @@ const Switch: React.FC<SwitchProps> = ({
       <div
         className={`
           w-8 h-5 flex items-center rounded-full p-1 transition-colors duration-300
-          ${checked ? "bg-indigo-500" : "bg-gray-300"}
+          ${checked ? "bg-gray-600" : "bg-gray-600 "}
           ${trackClassName}
         `}
         onClick={() => onChange(!checked)}
       >
         <div
           className={`
-            bg-white w-3 h-3 rounded-full shadow-md transform transition-transform duration-300
-            ${checked ? "translate-x-3" : "translate-x-0"}
+            bg-white dark:bg-gray-200 w-3 h-3 rounded-full shadow-md transform transition-transform duration-300
+            ${
+              checked
+                ? rtl
+                  ? "translate-x-3" // move left in RTL
+                  : "translate-x-3" // move right in LTR
+                : "translate-x-0"
+            }
             ${thumbClassName}
           `}
         />
