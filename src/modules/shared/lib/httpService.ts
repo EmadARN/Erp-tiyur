0c9f86ac0a386fffaf988e1476208b47d4d12a8d
RezaFarzipour <rezafafa78@gmail.com>
2025-08-10 23:10:39 +0330

    httpService and rtkQueryBase configs updated

--- src/modules/shared/lib/httpService.ts
+++ src/modules/shared/lib/httpService.ts
@@ -1,45 +1,51 @@
-import axios from "axios";
+import axios, { type AxiosInstance } from "axios";
 
-const app = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    withCredentials: false,
+const kernelApi = axios.create({
+  baseURL: import.meta.env.VITE_API_KERNEL_URL,
+  withCredentials: true,
 });
 
-app.interceptors.request.use(
+const saleApi = axios.create({
+  baseURL: import.meta.env.VITE_API_SALE_URL,
+  withCredentials: true,
+});
+
+const buyApi = axios.create({
+  baseURL: import.meta.env.VITE_API_BUY_URL,
+  withCredentials: true,
+});
+
+function setupInterceptors(apiInstance: AxiosInstance) {
+  apiInstance.interceptors.request.use(
     (res) => res,
     (err) => Promise.reject(err)
-);
+  );
 
-app.interceptors.response.use(
+  apiInstance.interceptors.response.use(
     (res) => res,
     async (err) => {
-        const originalConfig = err.config;
-        if (err.response?.status === 401 && !originalConfig._retry) {
-            originalConfig._retry = true;
-            try {
-                const { data } = await axios.get(
-                    `${import.meta.env.VITE_API_URL}authentication/refresh-token/`,
-                    { withCredentials: true }
-                );
-                if (data) return app(originalConfig);
-            } catch (error) {
-                return Promise.reject(error);
-            }
+      const originalConfig = err.config;
+      if (err.response?.status === 401 && !originalConfig._retry) {
+        originalConfig._retry = true;
+        try {
+          const { data } = await axios.get(
+            `${import.meta.env.VITE_API_KERNEL_URL}auth/refresh`,
+            { withCredentials: true }
+          );
+          if (data) return apiInstance(originalConfig);
+        } catch (error) {
+          return Promise.reject(error);
         }
-        return Promise.reject(err);
+      }
+      return Promise.reject(err);
     }
-);
+  );
+}
 
-// هم کل اینستنس و هم متدها رو اکسپورت می‌کنیم
-export { app };
+setupInterceptors(kernelApi);
+setupInterceptors(saleApi);
+setupInterceptors(buyApi);
 
-const http = {
-    get: app.get,
-    post: app.post,
-    delete: app.delete,
-    put: app.put,
-    patch: app.patch,
-    request: app.request,
-};
+const http = { kernelApi, saleApi ,buyApi};
 
 export default http;
--- src/modules/shared/lib/rtkQueryBase.ts
+++ src/modules/shared/lib/rtkQueryBase.ts
@@ -1,38 +1,38 @@
-import type { AxiosRequestConfig } from 'axios'
-import type { BaseQueryFn } from '@reduxjs/toolkit/query'
-import type { AxiosError } from 'axios'
-import http from './httpService' // از http استفاده می‌کنیم چون حالا request دارد
+import type { AxiosRequestConfig, AxiosInstance } from "axios";
+import type { AxiosError } from "axios";
 
-type Method = 'get' | 'post' | 'put' | 'patch' | 'delete'
+type Method = "get" | "post" | "put" | "patch" | "delete";
 
 export const axiosBaseQuery =
-  (): BaseQueryFn<
-    {
-      url: string
-      method: Method
-      data?: AxiosRequestConfig['data']
-      params?: AxiosRequestConfig['params']
-    },
-    unknown,
-    unknown
-  > =>
-    async ({ url, method, data, params }) => {
-      try {
-        const result = await http.request({
-          url,
-          method,
-          data,
-          params,
-        })
+  (apiInstance: AxiosInstance) =>
+  async ({
+    url,
+    method,
+    data,
+    params,
+  }: {
+    url: string;
+    method: Method;
+    data?: AxiosRequestConfig["data"];
+    params?: AxiosRequestConfig["params"];
+  }) => {
+    try {
+      const result = await apiInstance.request({
+        url,
+        method,
+        data,
+        params,
+        withCredentials: true,
+      });
 
-        return { data: result.data }
-      } catch (axiosError) {
-        const err = axiosError as AxiosError
-        return {
-          error: {
-            status: err.response?.status,
-            data: err.response?.data || err.message,
-          },
-        }
-      }
+      return { data: result.data };
+    } catch (axiosError) {
+      const err = axiosError as AxiosError;
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        },
+      };
     }
+  };
