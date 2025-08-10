336adb636fd36fb40c746ded3a63db7b73f4196f
RezaFarzipour <rezafafa78@gmail.com>
2025-08-10 23:13:49 +0330

    buyRoutes created

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,13 @@
 import { useRoutes } from "react-router-dom";
 import { salesRoutes } from "@/modules/sales/routes";
 import { authRoutes } from "@/modules/auth/routes";
+import { buysRoutes } from "@/modules/buys/routes";
 
 export const AppRoutes = () => {
   const routes = [
     ...salesRoutes,
-    ...authRoutes
+    ...authRoutes,
+    ...buysRoutes
   ];
 
   const element = useRoutes(routes);
