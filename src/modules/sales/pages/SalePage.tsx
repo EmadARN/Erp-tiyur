0ed1b86ec21e90015f0d18b42983a4baa15a3bca
Reza <rezabhm50@gmail.com>
2025-08-10 12:20:29 +0330

    update sale page with new dynamic component

--- src/modules/sales/pages/SalePage.tsx
+++ src/modules/sales/pages/SalePage.tsx
@@ -1,11 +1,32 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { DataTable } from "@/modules/shared/components/table/DataTable";
 import { Button } from "@/modules/shared/components/ui/Button";
 import SectionTitle from "@/modules/shared/components/ui/Sectiontitle";
 import Breadcrumb from "@/modules/shared/components/ui/Breadcrumb";
+import {CreateDialog} from "@/modules/shared/components/dialog__/CreateDialog.tsx";
+
+
+
 
 const SalePage = () => {
   const [selectedItem, setSelectedItem] = useState<any>(null);
+  const [filterData, setFilterData] = useState<boolean>([]);
+  const [createIndex, setCreateIndex] = useState<number | null>(null);
+
+  const [table_data, setTableData] = useState<any>([
+    {
+      username: "rezabh",
+      first_name: "Reza",
+      last_name: "Bhm",
+      role: "User",
+    },
+    {
+      username: "alij",
+      first_name: "Ali",
+      last_name: "JJJ",
+      role: "Admin",
+    },
+  ]);
 
    const breadcrumbItems = [
     { label: "Dashboard", href: "/dashboard" },
@@ -32,20 +53,81 @@ const SalePage = () => {
     },
   ];
 
-  const data = [
+  const table_filter = [
     {
-      username: "rezabh",
-      first_name: "Reza",
-      last_name: "Bhm",
-      role: "User",
+      label: 'price (Toman)',
+      type: 'range',
+      name: 'price-range',
+      max: 1000,
+      min: 10,
+      step: 10,
+      defaultValue: [100, 500] // پیش‌فرض بازه
     },
     {
-      username: "alij",
-      first_name: "Ali",
-      last_name: "JJJ",
-      role: "Admin",
+      name: 'location',
+      label: 'location',
+      type: 'autocomplete',
+      options: ['vanak', 'zanjan', 'fereshte'],
+      placeholder: 'search for location',
+      defaultValue: 'vanak'
+    },
+    {
+      name: 'sort-by',
+      label: 'sort by',
+      type: 'select-box',
+      options: ['max-price', 'low-price'],
+      defaultValue: 'max-price'
+    },
+    {
+      name: 'price-based',
+      label: 'price-based',
+      type: 'switch',
+      defaultValue: true
+    },
+    {
+      name: 'construction-based',
+      label: 'costruction select',
+      type: 'multi-select',
+      options: ['iran', 'dubai'],
+      defaultValue: ['iran']
+    },
+    {
+      name: 'price-form',
+      label: 'price',
+      type: 'range-box',
+      defaultValue: [200, 800]
+    }
+  ];
+  const create_dialog_configs = [
+    {
+      name: "title",
+      label: "title product",
+      type: "string-input",
+      defaultValue: "product-1",
+    },
+    {
+      name: "sort-by",
+      label: "sort by",
+      type: "select-box",
+      options: ["max-price", "low-price"],
+      defaultValue: "max-price",
+    },
+    {
+      name: "price-based",
+      label: "price-based",
+      type: "switch",
+      defaultValue: true,
+    },
+    {
+      name: "construction-based",
+      label: "costruction select",
+      type: "multi-select",
+      options: ["iran", "dubai"],
+      defaultValue: ["iran"],
     },
   ];
+
+
 
   const handleSearch = (query: string) => {
     console.log("در حال جستجو برای:", query);
@@ -55,12 +137,17 @@ const SalePage = () => {
     console.log("Add clicked");
   };
 
+  const OnCreate = () => {
+    console.log("Add OnCreate");
+    setCreateIndex(true)
+  };
+
   return (
     <div className="p-6 bg-white rounded-xl shadow-sm space-y-6 min-h-screen">
       {/* دکمه افزودن */}
       <div className="flex  justify-between gap-2 shrink-0 sm:flex-row">
         <SectionTitle title="Sale"/>
-        <Button variant="default" size="sm" onClick={onAdd}>
+        <Button variant="default" size="sm" onClick={OnCreate}>
           Add member
           <svg className="w-4 h-4 ml-2" fill="currentColor" viewBox="0 0 24 24">
             <path d="M19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z" />
@@ -72,8 +159,20 @@ const SalePage = () => {
         <Breadcrumb items={breadcrumbItems}/>
       </div>
 
-      {/* جدول داده‌ها */}
-      <DataTable tableHead={tableHead} data={data} />
+      <DataTable tableHead={tableHead} data={table_data} handleSearch={handleSearch}
+
+                 tableFilters={table_filter} filterData={filterData} setFilterData={setFilterData}
+      />
+
+      <CreateDialog
+          open={createIndex !== null}
+          onClose={() => setCreateIndex(null)}
+          onConfirm={() => {
+            if (createIndex !== null) OnCreate?.(createIndex);
+            setCreateIndex(null);
+          }}
+          configs={create_dialog_configs}
+      />
     </div>
   );
 };
