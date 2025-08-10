8f7820479980351f1fdb789a161d1c639508f0a6
RezaFarzipour <rezafafa78@gmail.com>
2025-08-10 23:09:07 +0330

    vite.config.ts updated for https

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,36 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// });
+
+
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import fs from 'fs';
 import path from 'path';
+import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem')),
     },
+    port: 5173,
   },
+     resolve: {
+     alias: {
+       '@': path.resolve(__dirname, './src'),
+     },
+   },
 });
