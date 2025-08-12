6c70f8c1866a514526ffccda9a9a5cc8c7472d1f
emad <arabanemad@gmail.com>
2025-08-13 00:35:57 +0330

    create Loading

---
+++ src/modules/shared/components/ui/Loading.tsx
@@ -0,0 +1,45 @@
+export default function AnimatedCenterBar() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "70vh",
+      }}
+    >
+      <div
+        style={{
+          width: "30%",
+          height: "3px",
+          backgroundColor: "#1111",
+          borderRadius: "4px",
+          overflow: "hidden",
+          position: "relative",
+        }}
+      >
+        <span
+          style={{
+            display: "block",
+            height: "100%",
+            width: "50%", // طول تکه متحرک
+            background: "linear-gradient(90deg, #444, #777)",
+            borderRadius: "4px",
+            position: "absolute",
+            animation: "loadingAnim 1.5s linear infinite",
+          }}
+        ></span>
+      </div>
+
+      {/* استایل انیمیشن */}
+      <style>
+        {`
+          @keyframes loadingAnim {
+            0% { left: -30%; }
+            100% { left: 100%; }
+          }
+        `}
+      </style>
+    </div>
+  );
+}
