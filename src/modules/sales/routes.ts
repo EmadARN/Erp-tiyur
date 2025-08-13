63e9639cb945c02ef8843b409c1f7399322fe6b5
RezaFarzipour <rezafafa78@gmail.com>
2025-08-13 22:46:00 +0330

    loadedProduct routes added

--- src/modules/sales/routes.ts
+++ src/modules/sales/routes.ts
@@ -2,6 +2,7 @@ import DashboardLayout from "@/layouts/DashboardLayout";
 import React from "react";
 import type { RouteObject } from "react-router-dom";
 import SalePage from "./pages/SalePage";
+import LoadedProductItemsPage from "./pages/LoadedProductItemsPage";
 
 
 
@@ -14,6 +15,10 @@ export const saleRoutes: RouteObject[] = [
       {
         path: "sale/product",
         element: React.createElement(SalePage),
+      },
+      {
+        path: "sale/loaded-product-item",
+        element: React.createElement(LoadedProductItemsPage),
       },
 
     ],
