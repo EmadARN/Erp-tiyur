22c2bc3e0fb63f5182f91f01fafec8319a774814
RezaFarzipour <rezafafa78@gmail.com>
2025-08-13 22:46:15 +0330

    loadedProductItemsIndex and loadedProductItemsType added

---
+++ src/modules/sales/model/loadedProductItemsTypes.ts
@@ -0,0 +1,53 @@
+export interface CarInfo {
+    driver: string;
+    car: string;
+  }
+  
+  export interface LoadedProductCar {
+    car: CarInfo;
+    create_at: { date: string; user: string };
+    level: string;
+    first_weight: { weight: number; date: { date: string; user: string } };
+    last_weight: { weight: number; date: { date: string; user: string } };
+    buyer: string;
+    entrance_date: { date: string; user: string };
+    exit_date: { date: string; user: string };
+    is_cancelled: { status: boolean; user_date: { date: string; user: string } };
+  }
+  
+  export interface LoadedProduct {
+    product: { product: string; product_owner: string };
+    created: { date: string; user: string };
+    price: number;
+    car: LoadedProductCar;
+    is_weight_base: boolean;
+  }
+  
+  export interface LoadedProductItem {
+    id: string;
+    weight: number;
+    number: number;
+    loaded_product: LoadedProduct;
+  }
+  
+  export interface CreateLoadedProductItemDto extends LoadedProductItem {}
+  
+  export interface LoadedProductItemsResponse {
+    data: LoadedProductItem[];
+  }
+  
+  export type FilterValue =
+    | [number, number]
+    | string
+    | number
+    | boolean
+    | (string | number)[];
+  
+  export interface Filter {
+    type: "range" | "range-box" | "switch" | "select-box" | "autocomplete" | "multi-select";
+    name: string;
+    value: FilterValue;
+  }
+  
+  export type FiltersRecord = Record<string, Filter>;
+  
