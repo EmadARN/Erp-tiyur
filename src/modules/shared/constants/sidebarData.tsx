beb78f2b7956f648572c3ab0f26a071ed730d894
emad <arabanemad@gmail.com>
2025-08-13 20:47:51 +0330

    update sidebarData

--- src/modules/shared/constants/sidebarData.tsx
+++ src/modules/shared/constants/sidebarData.tsx
@@ -12,69 +12,101 @@ export const navItems = [
 export const treeData = [
   {
     id: "1",
-    label: "Sales",
+    label: "Production",
     icon: <FaFolder />,
-    path: "/dashboard/sales",
+    path: "/dashboard/Production/product",
     children: [
       {
         id: "1.1",
-        label: "sales A",
+        label: "production A",
         icon: <FaFile />,
-        path: "/dashboard/sales/a",
+        path: "/dashboard/Production/a",
       },
       {
         id: "1.2",
+        label: "production B",
+        icon: <FaFile />,
+        path: "/dashboard/Production/b",
+      },
+    ],
+  },
+
+  {
+    id: "2",
+    label: "Sale",
+    icon: <FaFolder />,
+    path: "/dashboard/sale/product",
+    children: [
+      {
+        id: "2.1",
+        label: "sales A",
+        icon: <FaFile />,
+        path: "/dashboard/sale/a",
+      },
+      {
+        id: "2.2",
         label: "sales B",
         icon: <FaFile />,
-        path: "/dashboard/sales/b",
+        path: "/dashboard/sale/b",
       },
     ],
   },
   {
-    id: "2",
+    id: "3",
     label: "Buy and Orders",
     icon: <FaFolder />,
     path: "/dashboard/buy/product",
     children: [
       {
-        id: "2.1",
+        id: "3.1",
         label: "Bank Account",
         icon: <FaFile />,
         path: "/dashboard/buy/bank-account",
       },
       {
-        id: "2.2",
+        id: "3.2",
         label: "Product Order",
         icon: <FaFile />,
         path: "/dashboard/buy/product",
       },
       {
-        id: "2.3",
+        id: "3.3",
         label: "Invoice",
         icon: <FaFile />,
         path: "/dashboard/buy/invoice",
       },
       {
-        id: "2.4",
+        id: "3.4",
         label: "Payments",
         icon: <FaFile />,
         path: "/dashboard/buy/payment",
       },
       {
-        id: "2.5",
+        id: "3.5",
         label: "Purchase Order",
         icon: <FaFile />,
         path: "/dashboard/buy/purchase-order",
       },
-
     ],
   },
   {
-    id: "2",
-    label: "Documents",
+    id: "4",
+    label: "WareHouse",
     icon: <FaFolder />,
+    path: "/dashboard/warehouse/product",
     children: [
-      { id: "2.1", label: "Doc 1", icon: <FaFile />, path: "/doc - 1" },
+      {
+        id: "4.1",
+        label: "wareHouse A",
+        icon: <FaFile />,
+        path: "/dashboard/warehouse/a",
+      },
+      {
+        id: "4.2",
+        label: "wareHouse B",
+        icon: <FaFile />,
+        path: "/dashboard/warehouse/b",
+      },
     ],
   },
 ];
