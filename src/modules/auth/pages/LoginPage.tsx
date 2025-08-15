763e0e0b27092e3e5a11077731f2cf98cc642be7
RezaFarzipour <rezafafa78@gmail.com>
2025-08-15 19:06:46 +0330

    login page colors changed

--- src/modules/auth/pages/LoginPage.tsx
+++ src/modules/auth/pages/LoginPage.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom"; // 👈 اضافه شد
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../../assets/images/logo.png";
 import { useToast } from "@/modules/shared/hooks/use-toast";
 import TextInput from "@/modules/shared/components/ui/TextInput";
 import { useLoginMutation } from "../api/Login";
 
-const LoginPage: React.FC = () => {
+const LoginPage = () => {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const { showToast } = useToast();
@@ -13,23 +13,22 @@ const LoginPage: React.FC = () => {
 
   const [login, { isLoading }] = useLoginMutation();
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await login({ username, password }).unwrap();
       showToast("Login successful", "success");
-
       navigate("/dashboard");
-    } catch (err: any) {
+    } catch (err) {
       showToast(err?.data?.message || "Login failed", "error");
     }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-200 to-blue-400 px-4">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-600 to-teal-400 px-4">
       <div className="bg-white w-full max-w-4xl rounded-3xl shadow-lg overflow-hidden grid md:grid-cols-2">
         {/* Left Side */}
-        <div className="bg-gradient-to-b from-blue-600 to-blue-400 text-white p-8 flex flex-col justify-center items-center relative">
+        <div className="bg-gradient-to-b from-purple-600 to-teal-400 text-white p-8 flex flex-col justify-center items-center relative">
           <div className="absolute top-0 left-0 w-full h-full">
             <svg viewBox="0 0 1440 320" className="w-full h-full">
               <path
@@ -45,40 +44,40 @@ const LoginPage: React.FC = () => {
               <img src={logo} alt="Logo" className="w-80 h-full" />
             </div>
             <h2 className="text-2xl font-bold">
-              Welcome to Tiour Slaughterhouse ERP System
+              Welcome to Slaughter ERP System
             </h2>
             <p className="text-sm mt-2">
               Integrated management of slaughtering processes, warehousing, and
-              poultry distribution. Please log in to your account to continue.
+              product distribution. Please log in to continue.
             </p>
           </div>
         </div>
 
         {/* Right Side */}
         <div className="p-8 bg-white z-10">
-          <h1 className="text-primary text-2xl font-bold text-center mb-6">
-            Slaughterhouse Management Panel Login
+          <h1 className="text-purple-600 text-2xl font-bold text-center mb-6">
+            Slaughter ERP Management Panel Login
           </h1>
 
           <form onSubmit={handleSubmit} className="space-y-5">
             <div>
-              <label className="block text-dark text-sm mb-1">Username </label>
+              <label className="block text-gray-900 text-sm mb-1">Username</label>
               <TextInput
                 value={username}
                 onChange={setUserName}
                 inputType="text"
-                className="w-full px-4 py-2  focus:outline-none focus:ring-2 focus:ring-primary text-[#111]"
+                className="w-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600 text-gray-900"
                 placeholder="Enter your username"
               />
             </div>
 
             <div>
-              <label className="block text-dark text-sm mb-1">Password</label>
+              <label className="block text-gray-900 text-sm mb-1">Password</label>
               <TextInput
                 value={password}
                 onChange={setPassword}
                 inputType="password"
-                className="w-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary text-[#111]"
+                className="w-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600 text-gray-900"
                 placeholder="Enter your password"
               />
             </div>
@@ -86,14 +85,14 @@ const LoginPage: React.FC = () => {
             <button
               type="submit"
               disabled={isLoading}
-              className="w-full bg-blue-500 cursor-pointer hover:bg-blue-800 text-white py-2 rounded font-medium transition disabled:opacity-50"
+              className="w-full bg-purple-600 cursor-pointer hover:bg-purple-500 text-white py-2 rounded font-medium transition disabled:opacity-50"
             >
               {isLoading ? "Signing in..." : "Sign in"}
             </button>
           </form>
 
-          <div className="flex justify-between text-sm text-dark mt-4">
-            <Link to="#" className="hover:text-blue-600">
+          <div className="flex justify-between text-sm text-gray-900 mt-4">
+            <Link to="#" className="hover:text-purple-600">
               Forgot password?
             </Link>
           </div>
