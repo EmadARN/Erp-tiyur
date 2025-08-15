bc54f26f8e92ec7267ab20bbdaf7e51344572709
RezaFarzipour <rezafafa78@gmail.com>
2025-08-15 19:08:40 +0330

    all landing section imported in LandingPage component

---
+++ src/modules/landing/LandingPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './components/Header'
+import HeroSection from './components/HeroSection'
+import Features from './components/Features'
+import WhySlughter from './components/WhySlughter'
+import CtaSection from './components/CtaSection'
+import Footer from './components/Footer'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <Header/>
+        <HeroSection/>
+        <Features/>
+        <WhySlughter/>
+        <CtaSection/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default LandingPage
