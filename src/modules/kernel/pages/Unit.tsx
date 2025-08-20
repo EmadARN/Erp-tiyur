43685096604af7977b30782103959a7624ea1321
Reza <rezabhm50@gmail.com>
2025-08-20 12:55:20 +0330

    add unit page

---
+++ src/modules/kernel/pages/Unit.tsx
@@ -0,0 +1,176 @@
+import { useState } from "react";
+import { DataTable } from "@/modules/shared/components/table/DataTable";
+import { CreateDialog } from "@/modules/shared/components/dialogs/CreateDialog";
+import { useGetunitsDetailsQuery } from "../api/unitApi.ts";
+import PageHeader from "@/modules/shared/components/header/PageHeader";
+import { SearchInput } from "@/modules/shared/components/ui/SearchInput";
+import {
+  getCreateDialogConfigs,
+  getUpdateDialogConfigs,
+  tableFilter,
+  tableHead,
+} from "../model/unitIndex.ts";
+import Loading from "@/modules/shared/components/ui/Loading";
+import NoData from "@/modules/shared/components/ui/NoData";
+import { FiBox, FiFilter } from "react-icons/fi";
+import { Button } from "@/modules/shared/components/ui/Button";
+import { useKernelData } from "@/modules/shared/hooks/useKernelData";
+import { useunitData } from "../hooks/useunitData.ts";
+import { useunitActions } from "../hooks/useunitActions.ts";
+
+const unit = () => {
+  const [createIndex, setCreateIndex] = useState<number | null>(null);
+  const [isFilterDrawerOpen, setFilterDrawerOpen] = useState(false);
+
+  const {
+    kernelData,
+    isLoading: isLoadingKernel,
+    isError: isErrorKernel,
+  } = useKernelData();
+  const {
+    units,
+    isLoading: isLoadingBuyProducts,
+    filterData,
+    setFilterData,
+    handleFilterOnChange,
+    handleSearch,
+  } = useunitData();
+  const {
+    deleteHandler,
+    bulkDeleteHandler,
+    handleCreateConfirm,
+    handleUpdateConfirm,
+  } = useunitActions();
+
+  const breadcrumbItems = [
+    { label: "Dashboard", href: "/dashboard" },
+    { label: "Kernel", href: "/dashboard/kernel" },
+    { label: "unit" },
+  ];
+
+  const OnCreate = (index: number | null) => {
+    setCreateIndex(index);
+  };
+
+  const isLoading = isLoadingBuyProducts || isLoadingKernel;
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isErrorKernel) {
+    return (
+      <NoData
+        title="Error loading essential data"
+        description="Could not load products, owners, or other required data. Please try again later."
+        icon={<FiBox className="w-12 h-12 text-red-500" />}
+      />
+    );
+  }
+  const isKernelDataEmpty = false;
+
+
+
+  if (!units || units.length === 0) {
+    return (
+      <div className="p-2 sm:p-4 md:p-6 bg-white rounded-xl shadow-sm min-h-screen">
+        <PageHeader
+          title="units"
+          breadcrumbItems={breadcrumbItems}
+          onCreate={() => OnCreate(1)}
+          createLabel="Add unit"
+        />
+        <div className="flex flex-col md:flex-row items-center justify-between mb-4 gap-4">
+          <div className="w-full md:w-1/3">
+            <SearchInput
+              value={
+                typeof filterData.search === "string" ? filterData.search : ""
+              }
+              onSearch={handleSearch}
+            />
+          </div>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => setFilterDrawerOpen(true)}
+            className="w-full md:w-auto"
+          >
+            <FiFilter className="mr-2" />
+            Filters
+          </Button>
+        </div>
+        <NoData
+          title="No products found"
+          description="Try adjusting your filters or creating a new product."
+          icon={<FiBox className="w-12 h-12 text-blue-500" />}
+        />
+
+        <CreateDialog
+          open={createIndex !== null}
+          onClose={() => setCreateIndex(null)}
+          onConfirm={handleCreateConfirm}
+          configs={getCreateDialogConfigs(kernelData)}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-2 sm:p-4 md:p-6 bg-white rounded-xl shadow-sm min-h-screen">
+      <PageHeader
+        title="units"
+        breadcrumbItems={breadcrumbItems}
+        onCreate={() => OnCreate(1)}
+        createLabel="Add units"
+        isCreatingDisabled={isKernelDataEmpty || isLoadingKernel}
+      />
+
+      <div className="flex flex-col md:flex-row items-center justify-between my-4 md:my-8 gap-4">
+        <div className="w-full md:w-1/3">
+          <SearchInput
+            value={
+              typeof filterData.search === "string" ? filterData.search : ""
+            }
+            onSearch={handleSearch}
+          />
+        </div>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => setFilterDrawerOpen(true)}
+          className="w-full md:w-auto"
+        >
+          <FiFilter className="mr-2" />
+          Filters
+        </Button>
+      </div>
+
+      <DataTable
+        tableHead={tableHead}
+        data={units ?? []}
+        deleteHandler={deleteHandler}
+        bulkDeleteHandler={bulkDeleteHandler}
+        useGetBuyProductDetailsQuery={useGetunitsDetailsQuery}
+        tableFilters={tableFilter}
+        filterData={filterData}
+        setFilterData={setFilterData}
+        applyFilter={handleFilterOnChange}
+        updateDialogConfigs={getUpdateDialogConfigs(kernelData)}
+        onUpdateConfirm={handleUpdateConfirm}
+        showFilterButton={false}
+        isFilterDrawerOpen={isFilterDrawerOpen}
+        onFilterDrawerClose={() => setFilterDrawerOpen(false)}
+        onFilterIconClick={() => setFilterDrawerOpen(true)}
+      />
+
+      <CreateDialog
+        open={createIndex !== null}
+        onClose={() => setCreateIndex(null)}
+        onConfirm={handleCreateConfirm}
+        configs={getCreateDialogConfigs(kernelData)}
+      />
+    </div>
+  );
+};
+
+export default unit;
